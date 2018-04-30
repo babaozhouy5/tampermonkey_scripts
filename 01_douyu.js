3169c513b2123ec9fa9df994aae18dd9f5b467c0
babaozhouy5 <1609032563@qq.com>
2018-04-30 16:40:00 +0800

    init commit

---
+++ 01_douyu.js
@@ -0,0 +1,197 @@
+// ==UserScript==
+// @icon         https://www.douyu.com/favicon.ico
+// @name         斗鱼弹幕弹窗
+// @namespace    [url=mailto:nwglbbz@gmail.com]nwglbbz@gmail.com[/url]
+// @version      0.1
+// @description  斗鱼播放器内弹幕弹窗
+// @author       babaozhouy5
+// @match        https://www.douyu.com/*
+// @require      http://apps.bdimg.com/libs/jquery/1.7.2/jquery.min.js
+// @grant        GM_log
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    // https://stackoverflow.com/questions/7167085/on-append-do-something/7167210#7167210
+    // (function($) {
+        // var origAppend = $.fn.append;
+        // $.fn.append = function () {
+            // return origAppend.apply(this, arguments).trigger("append");
+        // };
+    // })(jQuery);
+
+    GM_addStyle(".moveBar {position:absolute;width:245px;height:330px;background:#08466570;z-index:500;border-radius:10px 10px 10px 10px;}");
+    GM_addStyle(".danmu_banner {cursor:move;padding:2px 0px 2px 0px;border-bottom:solid 1px #ada6a6;text-align:center;border-radius:10px 10px 0px 0px;}");
+    GM_addStyle('.danmu_content {width:240px;height:280px;overflow:auto;padding-left:5px;display:block;}');
+    GM_addStyle('.danmu_content::-webkit-scrollbar {width:8px;height:1px;}');
+    GM_addStyle('.danmu_content::-webkit-scrollbar-thumb {border-radius: 10px;-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);background: #273c4e47;}');
+    GM_addStyle('.danmu_content::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);border-radius: 10px;background: #ededed73;}');
+    GM_addStyle('.danmu_content_wrap {width:220px;word-wrap:break-word;}');
+    GM_addStyle('.danmu_footer {padding:0px 0px 4px 0px;border-top:solid 1px #ada6a6;text-align:center;border-radius:0px 0px 10px 10px;}}');
+    
+    // construct moveableDiv
+    $(function(){
+        var host_name = $('a.zb-name')[0].title;
+
+        // construct moveable div
+        var moveableDiv = "<div class='moveBar'>";
+
+        // danmu_banner
+        moveableDiv += "<div class='danmu_banner'>";
+        moveableDiv += "<font color='#ffffff'>欢迎来到</font>";
+        moveableDiv += "<font style='font-size:15px;font-style:italic;color:#ff1ed7'> "+host_name+" </font>";
+        moveableDiv += "<font color='#ffffff'>的直播间</font>";
+        moveableDiv += "</div>";
+
+        // danmu_content
+        moveableDiv += "<div class='danmu_content'><div class='danmu_content_wrap' /></div>";
+
+        // danmu_footer
+        moveableDiv += "<div class='danmu_footer'>";
+        moveableDiv += "<div id='close_div' style='width:10px;height:10px;margin:2px 2px 2px 5px;display:inline-block;float:left;text-align:center;vertical-align:middle;cursor:pointer;'><font style='color:#a99c9c;'>X</font></div>";
+        moveableDiv += "<font color='#ffffff'>长度:</font>";
+        moveableDiv += "<input id='max_danmu_len' type='text' style='width:40px;background:transparent;border:0px;color:#f7c3c3;' value='1000' />";
+        moveableDiv += "<font color='#ffffff'>热度:</font>";
+        moveableDiv += "<font id='hot' color='#f7c3c3' />";
+        moveableDiv += "<div id='min_div' style='width:10px;height:1px;margin:2px 5px 2px 2px;display:inline-block;float:right;border:solid #a99c9c;cursor:pointer;' />";
+        moveableDiv += "</div>";
+
+        $("div#js-room-video").prepend(moveableDiv);
+        
+        setInterval(function(){
+            $('font#hot')[0].innerText = $('span.hot-v')[0].innerText;
+        }, 2000);
+    });
+    
+    // add move ability to moveableDiv
+    $(function () {
+        $('div.danmu_banner').mousedown(
+            function (event) {
+                var isMove = true;
+                var abs_x = event.pageX - $('div.moveBar').position().left;
+                var abs_y = event.pageY - $('div.moveBar').position().top;
+                $(document).mousemove(function (event) {
+                    if (isMove) {
+                        var obj = $('div.moveBar');
+                        var moveX = event.pageX - abs_x;
+                        var moveY = event.pageY - abs_y;
+                        obj.css({ 'left': moveX, 'top': moveY });
+                    }
+                }).mouseup(
+                    function () {
+                        var obj = $('div.moveBar');
+
+                        var objLeft = obj.position().left;
+                        var objTop = obj.position().top;
+                        var selfHeight = obj[0].clientHeight;
+                        var selfWidth = obj[0].clientWidth;
+                        var parentHeight = obj[0].parentElement.clientHeight;
+                        var parentWidth = obj[0].parentElement.clientWidth;
+
+                        if(objLeft < 0){
+                            obj.css({ 'left': 0 });
+                        }
+                        if(objLeft > parentWidth - selfWidth){
+                            obj.css({ 'left': parentWidth - selfWidth });
+                        }
+                        if(objTop < 0){
+                            obj.css({ 'top': 0 });
+                        }
+                        if(objTop > parentHeight - selfHeight){
+                            obj.css({ 'top': parentHeight - selfHeight });
+                        }
+                        isMove = false;
+                    });
+        });
+
+        // minimize
+        // $('#min_div').hover(
+            // function(){
+                // $('#min_div').css("background-color","yellow");
+            // },
+            // function(){
+                // $('#min_div').css("background-color","pink");
+            // });
+        $('#min_div').click(
+            function (event) {
+                var dis_style = $('div.danmu_content')[0].style.display;
+                if(dis_style === "none"){
+                    $('div.danmu_content')[0].style.display = "block";
+                    $('div.danmu_content_wrap')[0].style.display = "block";
+                    $('div.moveBar')[0].style.height = "330px";
+                    $('div.moveBar')[0].style.background = "#08466570";
+                    $('#min_div')[0].style.height = "1px";
+                } else {
+                    $('div.danmu_content')[0].style.display = "none";
+                    $('div.danmu_content_wrap')[0].style.display = "none";
+                    $('div.moveBar')[0].style.height = "50px";
+                    $('div.moveBar')[0].style.background = "#dddddd45";
+                    $('#min_div')[0].style.height = "10px";
+                }
+            });
+
+        // close
+        // $('#close_div').hover(
+            // function(){
+                // $('#close_div').css("background-color","yellow");
+            // },
+            // function(){
+                // $('#close_div').css("background-color","pink");
+            // });
+        $('#close_div').click(
+            function (event) {
+                $('div.moveBar')[0].remove();
+                $('div.chat-cont-wrap').unbind("scroll");
+            });
+    });
+
+    $(function(){
+        var last_danmu = null;
+        //$('ul.c-list').bind("append", function(event) {
+        $('div.chat-cont-wrap').bind("scroll", function(event) {
+            // GM_log('success');
+            var danmu_list = $('ul.c-list')[1];
+            var len = danmu_list.children.length;
+            var new_danmu = danmu_list.children[len-1].innerText;
+
+            if(new_danmu != last_danmu){
+                var name, say, danmuDiv;
+                var name_color = '#03e6ff', say_color  = 'white', welcome_color = '#efed0b';
+                var items = new_danmu.split('：');
+                if(items.length>1){
+                    name = items[0];
+                    say  = items[1];
+                    danmuDiv = "<div class='danmu'>";
+                    danmuDiv += "<font size='2' color='"+name_color+"'>"+name+"：</font>";
+                    danmuDiv += "<font size='2' color='"+say_color+"'>"+say+"<br/></font></div>";
+                } else{
+                    danmuDiv = "<div class='danmu'>";
+                    danmuDiv += "<font size='2' color='"+welcome_color+"'>"+new_danmu+"<br/></font></div>";
+                }
+
+                var scrollTop = $("div.danmu_content")[0].scrollTop;            // xx
+                var containerHeight = $("div.danmu_content")[0].clientHeight;   // 280
+                var scrollHeight = $("div.danmu_content_wrap")[0].clientHeight; // xx
+                $("div.danmu_content_wrap").append(danmuDiv);
+
+                if( scrollTop + containerHeight  === scrollHeight){
+                    $("div.danmu_content")[0].scrollTop = $("div.danmu_content")[0].scrollHeight;
+                }
+
+                var max_danmu_len = parseInt($('#max_danmu_len').val());
+                //GM_log(max_danmu_len);
+                if($('.danmu_content_wrap div').size() > max_danmu_len){
+                    var need_remove = $('.danmu_content_wrap div').size() - max_danmu_len;
+                    $.each($('.danmu_content_wrap div:lt('+need_remove+')'), function(){
+                        //GM_log('del success!');
+                        $(this).remove();
+                    });
+                }
+                last_danmu = new_danmu;
+            }
+        });
+    });
+})();
---
+++ 02_scrollbar.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         滑动条样式
+// @namespace    [url=mailto:nwglbbz@gmail.com]nwglbbz@gmail.com[/url]
+// @version      0.1
+// @description  更改默认滑动条样式
+// @author       babaozhouy5
+// @match        *://*/*
+// @grant        GM_log
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var style = GM_getValue('scrollStyle') !== undefined ? GM_getValue('scrollStyle') : 0;
+    switch(style){
+        case 0:
+            GM_addStyle('body::-webkit-scrollbar {width:8px;height:1px;}');
+            GM_addStyle('body::-webkit-scrollbar-thumb {border-radius: 10px;-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);background: #273c4e47;}');
+            GM_addStyle('body::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);border-radius: 10px;background: #ededed73;}');
+            break;
+        case 1:
+            GM_addStyle('body::-webkit-scrollbar {width:8px;height:1px;}');
+            GM_addStyle('body::-webkit-scrollbar-thumb {border-radius: 10px;-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);background-color: #F90;background-image: -webkit-linear-gradient(45deg, rgba(255, 255, 255, .2) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, .2) 50%, rgba(255, 255, 255, .2) 75%, transparent 75%, transparent);}');
+            GM_addStyle('body::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);border-radius: 10px;background: #ededed73;}');
+            break;
+        case 2:
+            GM_addStyle('body::-webkit-scrollbar {width:8px;height:1px;}');
+            GM_addStyle('body::-webkit-scrollbar-thumb {border-radius: 10px;-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);background: #535353;}');
+            GM_addStyle('body::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);border-radius: 10px;background: #ededed73;}');
+            break;
+        default:
+            break;
+    }
+    
+    GM_registerMenuCommand('滚动条样式0', function(){
+        GM_setValue('scrollStyle', 0);
+        GM_addStyle('body::-webkit-scrollbar {width:8px;height:1px;}');
+        GM_addStyle('body::-webkit-scrollbar-thumb {border-radius: 10px;-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);background: #273c4e47;}');
+        GM_addStyle('body::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);border-radius: 10px;background: #ededed73;}');
+        });
+    
+    GM_registerMenuCommand('滚动条样式1', function(){
+        GM_setValue('scrollStyle', 1);
+        GM_addStyle('body::-webkit-scrollbar {width:8px;height:1px;}');
+        GM_addStyle('body::-webkit-scrollbar-thumb {border-radius: 10px;-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);background-color: #F90;background-image: -webkit-linear-gradient(45deg, rgba(255, 255, 255, .2) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, .2) 50%, rgba(255, 255, 255, .2) 75%, transparent 75%, transparent);}');
+        GM_addStyle('body::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);border-radius: 10px;background: #ededed73;}');
+        });
+    GM_registerMenuCommand('滚动条样式2', function(){
+        GM_setValue('scrollStyle', 2);
+        GM_addStyle('body::-webkit-scrollbar {width:8px;height:1px;}');
+        GM_addStyle('body::-webkit-scrollbar-thumb {border-radius: 10px;-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);background: #535353;}');
+        GM_addStyle('body::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);border-radius: 10px;background: #ededed73;}');
+        });
+})();
